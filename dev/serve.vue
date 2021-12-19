d17cd4dddc8db914e3b5c2e6a8e1829f87192637
webformulator <mddalam1@gmail.com>
2021-12-19 20:34:22 +0530

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+import Avatar from '@/avatar.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    Avatar
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <avatar />
+  </div>
+</template>
---
+++ src/avatar.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script>
+export default {
+  name: "Avatar",
+};
+</script>
+
+<style scoped></style>
