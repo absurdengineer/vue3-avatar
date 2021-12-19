71dd14c6498983b5ed5e5eac299a7a1bd033915f
webformulator <mddalam1@gmail.com>
2021-12-19 21:20:00 +0530

    vue3-avatar Component Created.

---
+++ Readme.md
@@ -0,0 +1,66 @@
+# vue3-avatar
+
+A user avatar component for vue3. By default it uses light colors for Text and dark colors for Background.
+
+This component is highly inspired from [vue-avatar](https://github.com/eliep/vue-avatar).
+
+This component computes the Initials from provided name with following rules:
+
+1. Splits the name by spaces and hyphens.
+2. Finds the first letter of each parts.
+3. Maximum length of Avatar letters is 3.
+4. If the name is splitted into more than 2 parts then picks the first letter of part1, part2 and last part.
+
+## Examples
+
+- **Tony** will become **T**
+- **Tony Stark** will become **TS**
+- **Tony Howard-Stark** will become **THS**
+- **Albert Tony Howard Stark** will become **ATS**
+
+## Installation
+
+`npm install vue3-avatar`
+
+## Usage
+
+vue3-avatar is very easy to use.
+
+### ES6
+
+```
+
+import Avatar from "vue3-avatar";
+
+export default {
+    ...
+    components : {
+        Avatar,
+        ...
+    },
+    ...
+}
+
+```
+
+After importing the component, you can use it in your templates as:
+
+```
+<avatar name="John Doe"></avatar>
+```
+
+## Props
+
+| Property   | Required | Type    | Default | Decription                                     |
+| ---------- | -------- | ------- | ------- | ---------------------------------------------- |
+| name       | true     | String  | -       | Name to compute Avatar letters                 |
+| color      | false    | String  | white   | Text color for Avatar letters                  |
+| background | false    | String  | navy    | Background color for Avatar                    |
+| size       | false    | Number  | 40      | Pixel size for Avatar (Same Height and Width)  |
+| inverted   | false    | Boolean | false   | To Invert the text color with Background color |
+| inline     | false    | Boolean | false   | To create inline Avatar                        |
+| rounded    | false    | Boolean | true    | Square or Rounded                              |
+
+## Creator
+
+This component has been created by [Md Dilshad Alam](https://github.com/webformulator)
--- src/avatar.vue
+++ src/avatar.vue
@@ -1,9 +1,273 @@
-<template></template>
+<template>
+  <div
+    :style="{
+      color: displayColor,
+      width: size + 'px',
+      height: size + 'px',
+      fontSize: fontSize + 'px',
+      background: displayBackground,
+      display: inline && 'inline-flex',
+      borderRadius: rounded && '50%',
+    }"
+    class="avatar noselect"
+  >
+    {{ displayName }}
+  </div>
+</template>
 
 <script>
+const lightColors = [
+  "#F0F8FF",
+  "#FAEBD7",
+  "#00FFFF",
+  "#7FFFD4",
+  "#F0FFFF",
+  "#F5F5DC",
+  "#FFE4C4",
+  "#FFEBCD",
+  "#DEB887",
+  "#5F9EA0",
+  "#7FFF00",
+  "#D2691E",
+  "#FF7F50",
+  "#6495ED",
+  "#FFF8DC",
+  "#00FFFF",
+  "#B8860B",
+  "#A9A9A9",
+  "#A9A9A9",
+  "#BDB76B",
+  "#FF8C00",
+  "#E9967A",
+  "#8FBC8F",
+  "#00CED1",
+  "#FF1493",
+  "#00BFFF",
+  "#1E90FF",
+  "#FFFAF0",
+  "#FF00FF",
+  "#DCDCDC",
+  "#F8F8FF",
+  "#FFD700",
+  "#DAA520",
+  "#808080",
+  "#808080",
+  "#ADFF2F",
+  "#F0FFF0",
+  "#FF69B4",
+  "#CD5C5C",
+  "#FFFFF0",
+  "#F0E68C",
+  "#E6E6FA",
+  "#FFF0F5",
+  "#7CFC00",
+  "#FFFACD",
+  "#ADD8E6",
+  "#F08080",
+  "#E0FFFF",
+  "#FAFAD2",
+  "#D3D3D3",
+  "#D3D3D3",
+  "#90EE90",
+  "#FFB6C1",
+  "#FFA07A",
+  "#20B2AA",
+  "#87CEFA",
+  "#B0C4DE",
+  "#FFFFE0",
+  "#00FF00",
+  "#32CD32",
+  "#FAF0E6",
+  "#FF00FF",
+  "#66CDAA",
+  "#BA55D3",
+  "#9370D8",
+  "#3CB371",
+  "#7B68EE",
+  "#00FA9A",
+  "#48D1CC",
+  "#F5FFFA",
+  "#FFE4E1",
+  "#FFE4B5",
+  "#FFDEAD",
+  "#FDF5E6",
+  "#FFA500",
+  "#FF4500",
+  "#DA70D6",
+  "#EEE8AA",
+  "#98FB98",
+  "#AFEEEE",
+  "#D87093",
+  "#FFEFD5",
+  "#FFDAB9",
+  "#CD853F",
+  "#FFC0CB",
+  "#DDA0DD",
+  "#B0E0E6",
+  "#FF0000",
+  "#BC8F8F",
+  "#FA8072",
+  "#F4A460",
+  "#FFF5EE",
+  "#C0C0C0",
+  "#87CEEB",
+  "#FFFAFA",
+  "#00FF7F",
+  "#D2B48C",
+  "#D8BFD8",
+  "#FF6347",
+  "#40E0D0",
+  "#EE82EE",
+  "#F5DEB3",
+  "#FFFFFF",
+  "#F5F5F5",
+  "#FFFF00",
+  "#9ACD32",
+];
+const darkColors = [
+  "#000000",
+  "#0000FF",
+  "#8A2BE2",
+  "#A52A2A",
+  "#DC143C",
+  "#00008B",
+  "#008B8B",
+  "#006400",
+  "#8B008B",
+  "#556B2F",
+  "#9932CC",
+  "#8B0000",
+  "#483D8B",
+  "#2F4F4F",
+  "#2F4F4F",
+  "#9400D3",
+  "#696969",
+  "#696969",
+  "#B22222",
+  "#228B22",
+  "#008000",
+  "#4B0082",
+  "#800000",
+  "#0000CD",
+  "#C71585",
+  "#191970",
+  "#000080",
+  "#808000",
+  "#6B8E23",
+  "#800080",
+  "#4169E1",
+  "#8B4513",
+  "#2E8B57",
+  "#A0522D",
+  "#6A5ACD",
+  "#708090",
+  "#708090",
+  "#4682B4",
+  "#008080",
+];
 export default {
   name: "Avatar",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+    },
+    background: {
+      type: String,
+    },
+    size: {
+      type: Number,
+    },
+    inverted: {
+      type: Boolean,
+      default: false,
+    },
+    inline: {
+      type: Boolean,
+      default: false,
+    },
+    rounded: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  computed: {
+    fontSize() {
+      const size = this.size || 40;
+      if (this.displayName.length == 1) return size / 2;
+      else if (this.displayName.length == 2) return size / 2.5;
+      if (this.displayName.length == 3) return size / 3;
+      else return 14;
+    },
+    displayName() {
+      let words = this.name.trim().split(/[- ]/);
+      words = words.filter((word) => word !== "");
+      if (words.length >= 3)
+        return (
+          words[0][0].toUpperCase() +
+          words[1][0].toUpperCase() +
+          words[words.length - 1][0].toUpperCase()
+        );
+      else if (words.length == 2)
+        return words[0][0].toUpperCase() + words[1][0].toUpperCase();
+      else if (words.length == 1) return words[0][0].toUpperCase();
+      else return "";
+    },
+    displayBackground() {
+      return this.background
+        ? this.background
+        : this.inverted
+        ? this.lightColor
+        : this.darkColor;
+    },
+    displayColor() {
+      return this.color
+        ? this.color
+        : this.inverted
+        ? this.darkColor
+        : this.lightColor;
+    },
+    asciiValue() {
+      const username = this.name.trim();
+      let ascii = 0;
+      for (let index = 0; index < username.length; index++)
+        ascii += username.charCodeAt(index);
+      return ascii;
+    },
+    darkColor() {
+      return darkColors[this.asciiValue % darkColors.length];
+    },
+    lightColor() {
+      return lightColors[this.asciiValue % lightColors.length];
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Domine:wght@700&display=swap");
+.avatar {
+  font-family: "Domine", serif;
+  color: white;
+  background: navy;
+  font-size: 14px;
+  width: 45px;
+  height: 45px;
+  border-radius: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 700;
+}
+.noselect {
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */
+}
+</style>
